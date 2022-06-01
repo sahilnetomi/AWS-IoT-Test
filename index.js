5f4d884a599a7a42cf5210f9de94898b5992b323
sahilnetomi <sahil@netomi.com>
2022-06-01 20:38:11 +0530

    Added STS and Meta data

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello World');
+const AWS = require('aws-sdk')
+
+// Using STS
+const STS = new AWS.STS();
+const params = {
+  DurationSeconds: 3600,
+  RoleArn: 'arn:aws:iam::817265962114:role/dev-ecs-task-IotChatWidget',
+  RoleSessionName: 'CHAT_WIDGET'
+};
+STS.assumeRole(params, (err, data) => {
+  if (err) {
+    console.log(err); // an error occurred
+  }
+  else {
+    console.log('DATA FROM STS ASSUME ROLE', data)
+  }  
+})
+
+// Using EC2 metadata
+const val = new AWS.EC2MetadataCredentials({
+  httpOptions: { timeout: 5000 }, // 5 second timeout
+  maxRetries: 10, // retry 10 times
+  retryDelayOptions: { base: 200 }, // see AWS.Config for information
+})
+console.log('DATA FROM EC2 METADATA', val)
