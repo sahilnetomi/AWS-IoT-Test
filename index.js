e50dbfe5c4bf955a876b658a6c2b11d353f8c97e
sahilnetomi <sahil@netomi.com>
2022-06-02 17:52:51 +0530

    Callback added

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const AWS = require('aws-sdk')
 
 // Using STS
-const STS = new AWS.STS();
-const params = {
-  DurationSeconds: 3600,
-  RoleArn: 'arn:aws:iam::721846126987:role/RnD-Terraform-Test',
-  RoleSessionName: 'CHAT_WIDGET'
-};
-STS.assumeRole(params, (err, data) => {
-  if (err) {
-    console.log(err); // an error occurred
-  }
-  else {
-    console.log('DATA FROM STS ASSUME ROLE', data)
-  }
-})
+// const STS = new AWS.STS();
+// const params = {
+//   DurationSeconds: 3600,
+//   RoleArn: 'arn:aws:iam::721846126987:role/RnD-Terraform-Test',
+//   RoleSessionName: 'CHAT_WIDGET'
+// };
+// STS.assumeRole(params, (err, data) => {
+//   if (err) {
+//     console.log(err); // an error occurred
+//   }
+//   else {
+//     console.log('DATA FROM STS ASSUME ROLE', data)
+//   }
+// })
 
 // Using EC2 metadata
 const val = new AWS.EC2MetadataCredentials({
   httpOptions: { timeout: 5000 }, // 5 second timeout
   maxRetries: 10, // retry 10 times
   retryDelayOptions: { base: 200 }, // see AWS.Config for information
+}).get((err, data) => {
+  if (err) console.log(err)
+  console.log(data)
 })
 console.log('DATA FROM EC2 METADATA', val)
