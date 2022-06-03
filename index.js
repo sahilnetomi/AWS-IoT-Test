537f261a4e89b0febc8f93d26f90d4a95cfafac6
sahilnetomi <sahil@netomi.com>
2022-06-03 13:08:09 +0530

    Callback added

--- index.js
+++ index.js
@@ -21,8 +21,12 @@ const val = new AWS.EC2MetadataCredentials({
   httpOptions: { timeout: 5000 }, // 5 second timeout
   maxRetries: 10, // retry 10 times
   retryDelayOptions: { base: 200 }, // see AWS.Config for information
-}).get((err, data) => {
-  if (err) console.log(err)
-  console.log(data)
 })
 console.log('DATA FROM EC2 METADATA', val)
+
+const data = new AWS.MetadataService({
+  httpOptions: { timeout: 5000 }, // 5 second timeout
+  maxRetries: 10, // retry 10 times
+  retryDelayOptions: { base: 200 }, // see AWS.Config for information
+})
+console.log('DATA FROM EC2 METADATA', data)
